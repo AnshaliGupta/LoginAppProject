3c3b46032c7e7f3b2dd4df61cc1bc862fdff15f8
Anshali <anshaligupta10@gmail.com>
2024-11-17 22:55:59 +0530

    Updated the Project with SignUp Page.

---
+++ app/src/main/java/com/anshali/demologinapp/activity/TermsAndConditionsActivity.java
@@ -0,0 +1,18 @@
+package com.anshali.demologinapp.activity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.anshali.demologinapp.R;
+
+public class TermsAndConditionsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.terms_conditions);
+    }
+}
